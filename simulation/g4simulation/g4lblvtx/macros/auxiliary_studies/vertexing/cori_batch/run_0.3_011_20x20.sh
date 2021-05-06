94d1621148e55882543c67048b22c18a5efdf954
Friederike Bock <friederike.bock@cern.ch>
2021-05-06 14:06:18 -0400

    restructed ALLsilicon directory

--- simulation/g4simulation/g4lblvtx/macros/auxiliary_studies/vertexing/cori_batch/run_0.3_011_20x20.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --image=docker:ddixit/fun4all:eicresearch
-#SBATCH --qos=shared
-#SBATCH --constraint=haswell
-#SBATCH --time=20:00
-#SBATCH --array=0-99
-shifter ./shifter.sh $SLURM_ARRAY_TASK_ID 100000 0 1 1 0.3 20
