9476e3d284ec2c53e167ce1ca92a7b1eeb9b52e4
Chris Pinkenburg <pinkenburg@bnl.gov>
2021-06-08 20:22:12 -0400

    Add Dirc Stacking Action to apply efficient efficiency

---
+++ simulation/g4simulation/g4eicdirc/G4EicDircStackingAction.h
@@ -0,0 +1,27 @@
+#ifndef G4EICDIRCSTACKINGACTION_H
+#define G4EICDIRCSTACKINGACTION_H
+
+#include <g4main/PHG4StackingAction.h>
+
+#include <Geant4/G4UserStackingAction.hh>
+
+#include <gsl/gsl_rng.h>
+
+class G4EicDircDetector;
+class TGraph;
+
+class G4EicDircStackingAction : public PHG4StackingAction
+{
+ public:
+  G4EicDircStackingAction(G4EicDircDetector* det);
+  ~G4EicDircStackingAction() override;
+
+  G4ClassificationOfNewTrack ClassifyNewTrack(const G4Track* aTrack) override;
+
+ private:
+  gsl_rng* RandomGenerator;
+  G4EicDircDetector* m_Detector = nullptr;
+  TGraph* fDetEff[2] = {nullptr};
+};
+
+#endif
