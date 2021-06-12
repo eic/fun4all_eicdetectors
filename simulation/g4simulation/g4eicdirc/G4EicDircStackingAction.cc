bf4c085c7829504352813007497833e914e5b74f
Chris Pinkenburg <pinkenburg@bnl.gov>
2021-06-12 12:36:52 -0400

    Add PrepareNewEvent() to reset counters at beginning of new event

--- simulation/g4simulation/g4eicdirc/G4EicDircStackingAction.cc
+++ simulation/g4simulation/g4eicdirc/G4EicDircStackingAction.cc
@@ -6,11 +6,14 @@
 
 #include <TGraph.h>
 
+#include <Geant4/G4OpticalPhoton.hh>
+#include <Geant4/G4Track.hh>
+#include <Geant4/G4VProcess.hh>
+
 #include <gsl/gsl_const.h>
 #include <gsl/gsl_randist.h>
 #include <gsl/gsl_rng.h>
 
-#include <Geant4/G4Track.hh>
 
 #include <iostream>
 #include <string>
@@ -19,8 +22,8 @@ G4EicDircStackingAction::G4EicDircStackingAction(G4EicDircDetector* detector)
   : PHG4StackingAction(detector->GetName())
   , m_Detector(detector)
 {
-  RandomGenerator = gsl_rng_alloc(gsl_rng_mt19937);
-  gsl_rng_set(RandomGenerator, PHRandomSeed());
+  m_RandomGenerator = gsl_rng_alloc(gsl_rng_mt19937);
+  gsl_rng_set(m_RandomGenerator, PHRandomSeed());
   // create a detector efficiency function:
   {
     // quantum efficiency data from Alex Britting, Jan 25, 2011
@@ -32,12 +35,12 @@ G4EicDircStackingAction::G4EicDircStackingAction(G4EicDircDetector* detector)
 
     Double_t eff_400[251] = {0, 0, 14.0, 14.8, 14.5, 14.9, 14.4, 14.2, 13.9, 14.6, 15.2, 15.7, 16.4, 16.9, 17.5, 17.7, 18.1, 18.8, 19.3, 19.8, 20.6, 21.4, 22.4, 23.1, 23.6, 24.1, 24.2, 24.6, 24.8, 25.2, 25.7, 26.5, 27.1, 28.2, 29.0, 29.9, 30.8, 31.1, 31.7, 31.8, 31.6, 31.5, 31.5, 31.3, 31.0, 30.8, 30.8, 30.4, 30.2, 30.3, 30.2, 30.1, 30.1, 30.1, 29.8, 29.9, 29.8, 29.7, 29.7, 29.7, 29.8, 29.8, 29.9, 29.9, 29.8, 29.9, 29.8, 29.9, 29.8, 29.7, 29.8, 29.7, 29.8, 29.6, 29.5, 29.7, 29.7, 29.8, 30.1, 30.4, 31.0, 31.3, 31.5, 31.8, 31.8, 31.9, 32.0, 32.0, 32.0, 32.0, 32.2, 32.2, 32.1, 31.8, 31.8, 31.8, 31.7, 31.6, 31.6, 31.7, 31.5, 31.5, 31.4, 31.3, 31.3, 31.2, 30.8, 30.7, 30.5, 30.3, 29.9, 29.5, 29.3, 29.2, 28.6, 28.2, 27.9, 27.8, 27.3, 27.0, 26.6, 26.1, 25.9, 25.5, 25.0, 24.6, 24.2, 23.8, 23.4, 23.0, 22.7, 22.4, 21.9, 21.4, 21.2, 20.7, 20.3, 19.8, 19.6, 19.3, 18.9, 18.7, 18.3, 17.9, 17.8, 17.8, 16.7, 16.5, 16.4, 16.0, 15.6, 15.6, 15.2, 14.9, 14.6, 14.4, 14.1, 13.8, 13.6, 13.3, 13.0, 12.8, 12.6, 12.3, 12.0, 11.9, 11.7, 11.5, 11.2, 11.1, 10.9, 10.7, 10.4, 10.3, 9.9, 9.8, 9.6, 9.3, 9.1, 9.0, 8.8, 8.5, 8.3, 8.3, 8.2, 7.9, 7.8, 7.7, 7.5, 7.3, 7.1, 6.9, 6.7, 6.6, 6.3, 6.2, 6.0, 5.8, 5.7, 5.6, 5.4, 5.2, 5.1, 4.9, 4.8, 4.6, 4.5, 4.4, 4.2, 4.1, 4.0, 3.8, 3.7, 3.5, 3.3, 3.2, 3.1, 3.0, 2.9, 2.5, 2.4, 2.4, 2.3, 2.3, 2.1, 1.8, 1.6, 1.5, 1.5, 1.6, 1.8, 1.9, 1.4, 0.8, 0.9, 0.8, 0.7, 0.6, 0.3, 0.3, 0.5, 0.3, 0.4, 0.3, 0.1, 0.2, 0.1, 0.2, 0.3, 0.0};
 
-    Double_t lambda[2][1000] = {{0}}, fEfficiencyR[2][1000] = {{0}}, fCollectionEff = 0.65;
+    Double_t lambda[2][1000] = {{0}}, fEfficiencyR[2][1000] = {{0}};
     // still need to convert from percent and cut values below credible limit
     for (auto i = 1; i < 1000; i++)
     {
       // Std QE (Photonis 2)
-      fCollectionEff = 0.95;
+      double fCollectionEff = 0.95;
       lambda[0][i] = 200 + i - 1;
       fEfficiencyR[0][i] = (i > 40 && i < 500) ? eff_std[i] * 0.01 * fCollectionEff : 0;
 
@@ -54,13 +57,28 @@ G4EicDircStackingAction::G4EicDircStackingAction(G4EicDircDetector* detector)
 
 G4EicDircStackingAction::~G4EicDircStackingAction()
 {
+  gsl_rng_free(m_RandomGenerator);
   delete fDetEff[0];
   delete fDetEff[1];
 }
 
 G4ClassificationOfNewTrack G4EicDircStackingAction::ClassifyNewTrack(const G4Track* aTrack)
 {
+//  std::cout << "calling stacking action" << std::endl;
   G4VPhysicalVolume* volume = aTrack->GetVolume();
+
+
+  if(aTrack->GetDefinition() == G4OpticalPhoton::OpticalPhotonDefinition())
+  { // particle is optical photon
+    if(aTrack->GetParentID()>0)
+    { // particle is secondary
+      if(aTrack->GetCreatorProcess()->GetProcessName() == "Scintillation")
+        fScintillationCounter++;
+      if(aTrack->GetCreatorProcess()->GetProcessName() == "Cerenkov")
+        fCerenkovCounter++;
+    }
+  }
+
   int whichactive = m_Detector->IsInDetector(volume);
   if (!whichactive)
   {
@@ -74,11 +92,19 @@ G4ClassificationOfNewTrack G4EicDircStackingAction::ClassifyNewTrack(const G4Tra
       return fKill;
     }
     Double_t lambda = 197.0 * 2.0 * M_PI / (aTrack->GetMomentum().mag() * 1.0E6);
-    Double_t ra = gsl_rng_uniform(RandomGenerator);
+    Double_t ra = gsl_rng_uniform(m_RandomGenerator);
     if (ra > fDetEff[1]->Eval(lambda))
     {
       return fKill;
     }
   }
   return fUrgent;
+}
+
+void G4EicDircStackingAction::PrepareNewEvent()
+{
+//  std::cout << "calling prepare new event" << std::endl;
+  fCerenkovCounter = 0;
+  fScintillationCounter = 0;
+  return;
 }
