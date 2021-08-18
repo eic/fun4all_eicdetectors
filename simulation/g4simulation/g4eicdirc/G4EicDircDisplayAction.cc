b919258f6f83b879a4448db6da4c1458fa85ec60
cdean-github <cameron.dean@cern.ch>
2021-08-18 18:04:11 -0400

    DIRC was trying to access a null pointer

--- simulation/g4simulation/g4eicdirc/G4EicDircDisplayAction.cc
+++ simulation/g4simulation/g4eicdirc/G4EicDircDisplayAction.cc
@@ -29,33 +29,37 @@ G4EicDircDisplayAction::~G4EicDircDisplayAction()
 void G4EicDircDisplayAction::ApplyDisplayAction(G4VPhysicalVolume *physvol)
 {
   // check if vis attributes exist, if so someone else has set them and we do nothing
-  if (m_MyVolume->GetVisAttributes())
+  for (auto it : m_LogicalVolumeMap)
   {
-    return;
+    m_MyVolume = it.first;
+    if (m_MyVolume->GetVisAttributes())
+    {
+      continue;
+    }
+    m_VisAtt = new G4VisAttributes();
+    if (m_Params->get_int_param("blackhole"))
+    {
+      PHG4Utils::SetColour(m_VisAtt, "BlackHole");
+      m_VisAtt->SetVisibility(false);
+      m_VisAtt->SetForceSolid(false);
+    }
+    else
+    {
+      PHG4Utils::SetColour(m_VisAtt, m_Params->get_string_param("material"));
+      m_VisAtt->SetVisibility(true);
+      m_VisAtt->SetForceSolid(true);
+    }
+    if (m_Colour)
+    {
+      m_VisAtt->SetColour(m_Colour->GetRed(),
+                          m_Colour->GetGreen(),
+                          m_Colour->GetBlue(),
+                          m_Colour->GetAlpha());
+      m_VisAtt->SetVisibility(true);
+      m_VisAtt->SetForceSolid(true);
+    }
+    m_MyVolume->SetVisAttributes(m_VisAtt);
   }
-  m_VisAtt = new G4VisAttributes();
-  if (m_Params->get_int_param("blackhole"))
-  {
-    PHG4Utils::SetColour(m_VisAtt, "BlackHole");
-    m_VisAtt->SetVisibility(false);
-    m_VisAtt->SetForceSolid(false);
-  }
-  else
-  {
-    PHG4Utils::SetColour(m_VisAtt, m_Params->get_string_param("material"));
-    m_VisAtt->SetVisibility(true);
-    m_VisAtt->SetForceSolid(true);
-  }
-  if (m_Colour)
-  {
-    m_VisAtt->SetColour(m_Colour->GetRed(),
-                        m_Colour->GetGreen(),
-                        m_Colour->GetBlue(),
-                        m_Colour->GetAlpha());
-    m_VisAtt->SetVisibility(true);
-    m_VisAtt->SetForceSolid(true);
-  }
-  m_MyVolume->SetVisAttributes(m_VisAtt);
   return;
 }
 
