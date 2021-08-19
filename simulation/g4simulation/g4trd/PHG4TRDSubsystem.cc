98800a96731f15e2c227321bb8ac9e491145ae4a
sourav <sourav.pheonix@gmail.com>
2021-08-19 00:31:07 -0400

    TRD in Hadron end cap-version 1

---
+++ simulation/g4simulation/g4trd/PHG4TRDSubsystem.cc
@@ -0,0 +1,141 @@
+#include "PHG4TRDSubsystem.h"
+#include "PHG4TRDDetector.h"
+#include "PHG4TRDSteppingAction.h"
+
+#include <phparameter/PHParameters.h>
+
+#include <g4main/PHG4DisplayAction.h>  // for PHG4DisplayAction
+#include <g4main/PHG4HitContainer.h>
+#include <g4main/PHG4SteppingAction.h>  // for PHG4SteppingAction
+#include <g4main/PHG4Utils.h>
+
+#include <phool/PHCompositeNode.h>
+#include <phool/PHIODataNode.h>    // for PHIODataNode
+#include <phool/PHNode.h>          // for PHNode
+#include <phool/PHNodeIterator.h>  // for PHNodeIterator
+#include <phool/PHObject.h>        // for PHObject
+#include <phool/getClass.h>
+#include <phool/recoConsts.h>
+
+#include <cmath>     // for NAN
+#include <iostream>  // for operator<<, basic_ostream, endl
+#include <sstream>
+
+class PHG4Detector;
+class PHG4SteppingAction;
+
+using namespace std;
+
+PHG4TRDSubsystem::PHG4TRDSubsystem(const std::string &na, const int lyr)
+  : PHG4DetectorSubsystem(na, lyr),
+    m_Detector(nullptr),
+    m_SteppingAction(nullptr)
+{
+
+  InitializeParameters();
+}
+
+PHG4TRDSubsystem::~PHG4TRDSubsystem()
+{
+
+}
+
+//_______________________________________________________________________
+int PHG4TRDSubsystem::InitRunSubsystem(PHCompositeNode *topNode)
+{
+
+  PHNodeIterator iter(topNode);
+  PHCompositeNode *dstNode = dynamic_cast<PHCompositeNode *>(iter.findFirst("PHCompositeNode", "DST"));
+  // create detector
+  m_Detector = new PHG4TRDDetector(this, topNode, GetParams(), Name(), GetLayer());
+  m_Detector->SuperDetector(SuperDetector());
+  m_Detector->OverlapCheck(CheckOverlap());
+
+  string nodename;
+  nodename = "G4HIT_" + SuperDetector();
+  string nodes;
+
+  if (GetParams()->get_int_param("active"))
+    {
+      PHG4HitContainer *TRD_hits = findNode::getClass<PHG4HitContainer>(topNode, nodename);
+      
+      if (!TRD_hits)
+	{
+	  //dstNode->addNode(new PHIODataNode<PHObject>(TRD_hits = new PHG4HitContainer(nodename), nodename, "PHObject"));
+	  TRD_hits = new PHG4HitContainer(nodename);
+	  PHIODataNode<PHObject>* hitNode = new PHIODataNode<PHObject>(TRD_hits, nodename, "PHObject");
+	  dstNode->addNode(hitNode);
+	  // nodes.insert(nodename);
+	}
+
+      //Stepping action
+      auto *tmp = new PHG4TRDSteppingAction(this, m_Detector, GetParams());
+      tmp->HitNodeName(nodename);
+      m_SteppingAction = tmp;
+    }
+  else if (GetParams()->get_int_param("blackhole"))
+    {
+      m_SteppingAction = new PHG4TRDSteppingAction(this, m_Detector, GetParams());
+    }
+    /*
+  if (m_SteppingAction)
+    {
+      (dynamic_cast<PHG4TRDSteppingAction *>(m_SteppingAction))->SaveAllHits(m_SaveAllHitsFlag);
+    }
+    */
+  return 0;
+}
+
+int PHG4TRDSubsystem::process_event(PHCompositeNode *topNode)
+{
+  // pass top node to stepping action so that it gets
+  // relevant nodes needed internally
+  if (m_SteppingAction)
+    {
+      m_SteppingAction->SetInterfacePointers(topNode);
+    }
+  return 0;
+}
+
+void PHG4TRDSubsystem::SetDefaultParameters()
+{
+  set_default_double_param("ThicknessZ", 35.);
+  set_default_double_param("RIn", 20.);
+  set_default_double_param("ROut", 200.);
+  set_default_double_param("PosZ", 30.);
+  set_default_double_param("det_RIn", 20.);
+  set_default_double_param("det_ROut", 200.);
+  set_default_int_param("use_g4steps", 1);
+  
+  // place holder, will be replaced by world material if not set by other means (macro)
+  set_default_string_param("material", "G4_AIR");
+}
+
+PHG4Detector *
+PHG4TRDSubsystem::GetDetector(void) const
+{
+  return m_Detector;
+}
+
+
+void PHG4TRDSubsystem::Print(const string &what) const
+{
+  cout << Name() << " Parameters: " << endl;
+  if (!BeginRunExecuted())
+  {
+    cout << "Need to execute BeginRun() before parameter printout is meaningful" << endl;
+    cout << "To do so either run one or more events or on the command line execute: " << endl;
+    cout << "Fun4AllServer *se = Fun4AllServer::instance();" << endl;
+    cout << "PHG4Reco *g4 = (PHG4Reco *) se->getSubsysReco(\"PHG4RECO\");" << endl;
+    cout << "g4->InitRun(se->topNode());" << endl;
+    cout << "PHG4TRDSubsystem *trd = (PHG4TRDSubsystem *) g4->getSubsystem(\"" << Name() << "\");" << endl;
+    cout << "trd->Print()" << endl;
+    return;
+  }
+  GetParams()->Print();
+  if (m_SteppingAction)
+  {
+    m_SteppingAction->Print(what);
+  }
+  return;
+}
---
+++ simulation/g4simulation/g4trd/PHG4TRDSubsystem.h
@@ -0,0 +1,62 @@
+#ifndef G4DETECTORS_PHG4TRDSUBSYSTEM_H
+#define G4DETECTORS_PHG4TRDSUBSYSTEM_H
+
+
+#include <g4detectors/PHG4DetectorSubsystem.h>
+
+#include <array>   // for array
+#include <string>  // for string
+
+class PHCompositeNode;
+class PHG4TRDDetector;
+class PHG4Detector;
+class PHG4SteppingAction;
+
+class PHG4TRDSubsystem : public PHG4DetectorSubsystem
+{
+public:
+//! constructor
+PHG4TRDSubsystem(const std::string& name = "TRD", const int layer = 0);
+  
+//! destructor
+~PHG4TRDSubsystem(void) override;
+  
+  
+int InitRunSubsystem(PHCompositeNode*) override ;
+  
+//! event processing
+/*!
+  get all relevant nodes from top nodes (namely hit list)
+  and pass that to the stepping action
+*/
+int process_event(PHCompositeNode*) override;
+  
+//! Print info (from SubsysReco)
+void Print(const std::string& what = "ALL") const override;
+
+//! accessors (reimplemented)
+PHG4Detector* GetDetector(void) const override;
+PHG4SteppingAction* GetSteppingAction(void) const override { return m_SteppingAction; }
+
+// this method is used to check if it can be used as mothervolume
+// Subsystems which can be mothervolume need to implement this
+// and return true
+bool CanBeMotherSubsystem() const override { return true; }
+
+private:
+void SetDefaultParameters() override ;
+  
+//! detector geometry
+/*! derives from PHG4Detector */
+PHG4TRDDetector* m_Detector = nullptr;
+
+//! particle tracking "stepping" action
+/*! derives from PHG4SteppingActions */
+PHG4SteppingAction* m_SteppingAction = nullptr;
+
+  
+bool m_SaveAllHitsFlag = false;
+  
+  
+};
+#endif  // G4DETECTORS_PHG4TRDSUBSYSTEM_H
