657d95188b8780e110618b1e6f856c1a992a1463
jns <jns@pop-os.localdomain>
2021-09-13 19:49:15 -0400

    modified:   simulation/g4simulation/g4eiccalos/PHG4BarrelEcalDetector.cc 	modified:   simulation/g4simulation/g4eiccalos/PHG4BarrelEcalDetector.h 	modified:   simulation/g4simulation/g4eiccalos/PHG4BarrelEcalSubsystem.cc

--- simulation/g4simulation/g4eiccalos/PHG4BarrelEcalSubsystem.cc
+++ simulation/g4simulation/g4eiccalos/PHG4BarrelEcalSubsystem.cc
@@ -147,6 +147,11 @@ void PHG4BarrelEcalSubsystem::SetDefaultParameters()
   set_default_double_param("cone2_h",20.5);
   set_default_double_param("cone2_dz",25.5);
   set_default_double_param("thickness_wall",0.1);
+  set_default_double_param("silicon_width_half",0.05);
+  set_default_double_param("kapton_width_half",0.05);
+  set_default_double_param("SIO2_width_half",0.05);
+  set_default_double_param("Carbon_width_half",1.);
+  set_default_double_param("support_length",1.5);
 
   return;
 }
